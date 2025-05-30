f89267ac6bfd889fc41514422a02c0abe0459fe4
Jayashree Sarangi (NTE-907) <jayashree.sadangi@hyscaler.com>
2025-05-30 12:47:53 +0530

    fix:ui

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,14 +1,16 @@
 export const Header = () => {
   return (
-    <header className="text-center mb-10 group">
-      <div className="inline-block relative">
-        <h1 className="text-5xl font-mono font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-cyan-400 mb-3 tracking-tighter">
-          <span className="blink">$</span> TOKEN_CONVERTER
+    <header className="text-center mb-12">
+      <div className="inline-block">
+        <h1 className="text-4xl md:text-5xl font-mono font-semibold text-green-400 mb-3">
+          <span className="ml-2  ">Token </span>
+       
+          <span className="text-purple-400 animate-pulse">Converter</span>
         </h1>
-        <div className="absolute -bottom-1 left-0 w-full h-0.5 bg-gradient-to-r from-green-400/0 via-green-400 to-cyan-400/0 group-hover:via-purple-400 transition-all duration-300" />
+        <div className="h-[2px] w-full bg-gradient-to-r from-transparent via-green-400 to-transparent" />
       </div>
-      <p className="text-gray-400 font-mono text-sm tracking-wider pt-5">
-        [JSON ⇄ TOKEN] • [JWT|BASE64|HEX] • [ENCRYPT/DECRYPT]
+      <p className="text-gray-400 font-mono text-sm md:text-base tracking-wider mt-4">
+        JSON ↔ JWT ↔ Base64 ↔ Hex ↔ URL
       </p>
     </header>
   );
